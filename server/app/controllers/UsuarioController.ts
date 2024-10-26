dd9444234f9e9cf6337513fae12065caf61296b5
Luchamas <lucasmabrahao10@gmail.com>
2024-10-26 14:19:00 -0300

    Endereço com Usuario create

--- server/app/controllers/UsuarioController.ts
+++ server/app/controllers/UsuarioController.ts
@@ -1,37 +1,47 @@
 /* eslint-disable prettier/prettier */
 import Usuario from '#models/usuario';
+import db from '@adonisjs/lucid/services/db'
 import hash from '@adonisjs/core/services/hash'
-import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http';
 
 export default class UsuarioController {
 
   public async store({ request, response }: HttpContext) {
-    const userData = request.only([
-      'nome',
-      'cpf',
-      'data_nascimento',
-      'sexo',
-      'escolaridade',
-      'ocupacao',
-      'email',
-      'senha',
-      'estado_civil',
-      'renda_mensal',
-      'id_monitor', 
-    ]);
-
-
-    userData.senha = await hash.make(userData.senha);
-
     try {
-      const usuario = await Usuario.create(userData);
+
+      const userData = request.only([
+        'nome',
+        'cpf',
+        'data_nascimento',
+        'sexo',
+        'escolaridade',
+        'ocupacao',
+        'email',
+        'senha',
+        'estado_civil',
+        'renda_mensal',
+        'id_monitor', 
+      ]);
+    
+    
+      userData.senha = await hash.make(userData.senha);
+
+      const usuario = await Usuario.create({...userData});
+
+      const enderecoData = request.input('endereco');
+      if (enderecoData) {
+        enderecoData.usuario_id = usuario.id; 
+        await db
+        .table('endereco') 
+        .insert(enderecoData)      
+        }
+
       return response.created({ success: true, message: 'Usuário criado com sucesso', usuario });
     } catch (error) {
-      return response.badRequest({success: false, message: 'Erro ao criar o usuário', error: error.message || error, });
+      return response.badRequest({ success: false, message: 'Erro ao criar o usuário', error: error.message || error });
     }
   }
 
- 
    async index({ response }: HttpContext) {
     const usuarios = await Usuario.all();
     return response.ok({ success: true, usuarios });
